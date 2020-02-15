8b29eb6733077d50df12c2f9f50171c06670cf42
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2020-02-15 18:18:13 +1100

    Initial commit with PoC code.

---
+++ LegoStickers/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.IO;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.Drawing;
+
+namespace LegoStickers
+{
+    public static class WTableCellExtensions
+    {
+        public static void SetContent(this WTableCell cell, string text, float fontSize, Color color)
+        {
+            var textRange = cell.AddParagraph().AppendText(text);
+            textRange.CharacterFormat.FontName = "Calibri";
+            textRange.CharacterFormat.Bold = true;
+            textRange.CharacterFormat.FontSize = fontSize;
+            textRange.CharacterFormat.TextColor = color;
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var doc = new WordDocument();
+
+            var section = doc.AddSection();
+            var table = section.AddTable();
+            table.ResetCells(6, 3);
+
+            table[0, 0].SetContent("2445", 18, Color.Black);
+            table[1, 0].SetContent("Plate 2 x 12", 12, Color.FromArgb(112, 173, 71));
+            table[2, 0].SetContent("Reddish Brown", 12, Color.FromArgb(68, 114, 196));
+            table[3, 0].SetContent("Plates", 12, Color.FromArgb(237, 125, 49));
+            table[4, 0].SetContent("4225700", 12, Color.Black);
+
+            using (var fs = File.OpenRead("2445.png"))
+            {
+                var paragraph = table[5, 0].AddParagraph();
+                paragraph.ParagraphFormat.HorizontalAlignment = HorizontalAlignment.Center;
+                var picture = paragraph.AppendPicture(fs);
+                picture.Height = 142;
+                picture.Width = 142;
+                table[5, 0].AddParagraph();
+                table[5, 0].CellFormat.VerticalAlignment = VerticalAlignment.Middle;
+            }
+
+            for (var row = 0; row < 5; row++)
+            {
+                table[row, 0].CellFormat.Borders.Bottom.BorderType = BorderStyle.Cleared;
+            }
+
+            for (var row = 1; row < 6; row++)
+            {
+                table[row, 0].CellFormat.Borders.Top.BorderType = BorderStyle.Cleared;
+            }
+            
+//            table[0, 0].CellFormat.Borders.Bottom.BorderType = BorderStyle.Cleared;
+//            table[1, 0].CellFormat.Borders.Top.BorderType = BorderStyle.Cleared;
+            
+
+//            IWTextRange textRange = section.AddParagraph().AppendText("Price Details");
+//            textRange.CharacterFormat.FontName = "Arial";
+//            textRange.CharacterFormat.FontSize = 12;
+//            textRange.CharacterFormat.Bold = true;
+//            textRange.CharacterFormat.TextColor = Color.Black;
+//            
+//section.AddParagraph();
+////Adds a new table into Word document
+//IWTable table = section.AddTable();
+////Specifies the total number of rows & columns
+//table.ResetCells(3, 2);
+////Accesses the instance of the cell (first row, first cell) and adds the content into cell
+//textRange = table[0, 0].AddParagraph().AppendText("Item");
+//textRange.CharacterFormat.FontName = "Arial";
+//textRange.CharacterFormat.FontSize = 12;
+//textRange.CharacterFormat.Bold = true;
+////Accesses the instance of the cell (first row, second cell) and adds the content into cell
+//textRange = table[0, 1].AddParagraph().AppendText("Price($)");
+//textRange.CharacterFormat.FontName = "Arial";
+//textRange.CharacterFormat.FontSize = 12;
+//textRange.CharacterFormat.Bold = true;
+////Accesses the instance of the cell (second row, first cell) and adds the content into cell
+//textRange = table[1, 0].AddParagraph().AppendText("Apple");
+//textRange.CharacterFormat.FontName = "Arial";
+//textRange.CharacterFormat.FontSize = 10;
+////Accesses the instance of the cell (second row, second cell) and adds the content into cell
+//textRange = table[1, 1].AddParagraph().AppendText("50");
+//textRange.CharacterFormat.FontName = "Arial";
+//textRange.CharacterFormat.FontSize = 10;
+////Accesses the instance of the cell (third row, first cell) and adds the content into cell
+//textRange = table[2, 0].AddParagraph().AppendText("Orange");
+//textRange.CharacterFormat.FontName = "Arial";
+//textRange.CharacterFormat.FontSize = 10;
+////Accesses the instance of the cell (third row, second cell) and adds the content into cell
+//textRange = table[2, 1].AddParagraph().AppendText("30");
+//textRange.CharacterFormat.FontName = "Arial";
+//textRange.CharacterFormat.FontSize = 10;
+
+using (var fs = File.OpenWrite("stickers.docx"))
+            {
+                doc.Save(fs, FormatType.Docx);
+                doc.Close();
+            }
+        }
+    }
+}
---
+++ LegoStickers/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LegoStickers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LegoStickers")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("B5EDE7B2-6F13-48DD-A4CB-415AE030861E")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
